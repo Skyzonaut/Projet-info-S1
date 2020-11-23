2eacf1983a0e410a8ab9a4f495ca7e85625b9081
yohan <yohan.folliot@gmail.com>
2020-11-23 15:57:48 +0100

    commit avec création classe

---
+++ Main/plateau.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from random import randrange
+from pprint import *
+
+class plateau:
+
+	def __init__(self):
+
+		#Creation du plateau
+		self.matrice = {}
+
+		for x in range(1,9):
+			for y in range(1,9):
+				self.matrice[(x,y)] = ""
+
+		def apercu():
+			final = ""
+			for x in range(1, 9):
+				line = "|"
+				for y in range(1, 9):
+					line += " " + self.matrice[(x,y)] + " |"
+				final += line+'\n'
+			return final
+
+plateau = plateau()
+pprint(plateau.matrice)
+plateau.apercu()
--- Main/test_tkinker.py
+++ Main/test_tkinker.py
@@ -1,32 +1,12 @@
 from tkinter import *
 from random import randrange
 
-def drawline():
-
-    global x1, y1, x2, y2, coul
-    plateau.create_line(x1, y1, x2, y2, width=2, fill=coul)
-
-    y2, y1 = y2 + 10, y1 - 10
-
-def drawline2():
-
-    global x3, y3, x4, y4, x5, y5, x6, y6, coul2
-    plateau.create_line(x3, y3, x4, y4, width=2, fill=coul2)
-    plateau.create_line(x5, y5, x6, y6, width=2, fill=coul2)
-
-def changecolor():
-
-    global coul
-    pal=['purple', 'cyan', 'maroon', 'green', 'red', 'blue', 'orange', 'yellow']
-    c = randrange(8)
-    coul = pal[c]
-
+def consolePrint(text):
+    console.create_text(60,10,text=text)
 
 if __name__ == '__main__':
 
     # Micellious
-    coul='white'
-    coul2='red'
     fenX = 1300
     fenY = 850
 
@@ -36,13 +16,16 @@ if __name__ == '__main__':
 
     plateau = Canvas(fen, bg='white', width=950, height=int(fenY))
     plateau.pack(side=LEFT)
-    console = Canvas(fen, bg='white', width=(fenX-950), height=int(fenY))
-    plateau.pack(side=LEFT)
+    console = Canvas(fen, bg='grey', width=(fenX-950), height=int(fenY))
+    console.pack(side=LEFT)
+    # console.create_text(60,10,text="TA mère la chienne")
+    # consolePrint("ta mère")
 
     pos = 0
-    while pos <= plateau.winfo_reqwidth() / 8:
-        print(plateau.winfo_reqwidth())
-        plateau.create_line(pos, 0, pos, plateau.winfo_reqheight(), width=2, fill=coul2)
+    while pos <= plateau.winfo_reqwidth():
+        consolePrint(plateau.winfo_reqwidth())
+        plateau.create_line(pos, 0, pos, plateau.winfo_reqheight(), width=2, fill="black")
+        plateau.create_line(0, pos, plateau.winfo_reqwidth(), pos, width=2, fill="black")
         pos += plateau.winfo_reqwidth() / 8
     fen.mainloop()
 
