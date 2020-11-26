2c942c3638be79b0868946e771c7502c360d0234
yohan <yohan.folliot@gmail.com>
2020-11-26 17:00:15 +0100

    Ajout d'une fonction pour déplacer le contenu d'une case à une autre, correction du placement des pions et un tantinet de doc.

--- Main/project/classe/plateau.py
+++ Main/project/classe/plateau.py
@@ -22,7 +22,9 @@ class plateau:
 		==============================================================================
 		"""
 
+		# Matrice contenant les cases et dans ces cases leur contenu
 		self.matrice = {}
+
 		for x in range(1,9):
 			for y in range(1,9):
 				pion = self.setDeJeu.getFreePion("empty","")
@@ -34,16 +36,16 @@ class plateau:
 
 		for i in range(1,9):
 			pion = self.setDeJeu.getFreePion("pion","noir")
-			self.matrice[(2,i)] = pion
+			self.matrice[(i,2)] = pion
 
 		self.matrice[(1, 1)] = self.setDeJeu.getFreePion("tour","noir")
-		self.matrice[(1, 2)] = self.setDeJeu.getFreePion("cavalier","noir")
-		self.matrice[(1, 3)] = self.setDeJeu.getFreePion("fou","noir")
-		self.matrice[(1, 4)] = self.setDeJeu.getFreePion("roi","noir")
-		self.matrice[(1, 5)] = self.setDeJeu.getFreePion("reine","noir")
-		self.matrice[(1, 6)] = self.setDeJeu.getFreePion("fou","noir")
-		self.matrice[(1, 7)] = self.setDeJeu.getFreePion("cavalier","noir")
-		self.matrice[(1, 8)] = self.setDeJeu.getFreePion("tour","noir")
+		self.matrice[(2, 1)] = self.setDeJeu.getFreePion("cavalier","noir")
+		self.matrice[(3, 1)] = self.setDeJeu.getFreePion("fou","noir")
+		self.matrice[(4, 1)] = self.setDeJeu.getFreePion("reine","noir")
+		self.matrice[(5, 1)] = self.setDeJeu.getFreePion("roi","noir")
+		self.matrice[(6, 1)] = self.setDeJeu.getFreePion("fou","noir")
+		self.matrice[(7, 1)] = self.setDeJeu.getFreePion("cavalier","noir")
+		self.matrice[(8, 1)] = self.setDeJeu.getFreePion("tour","noir")
 
 		# ===============================================================================================
 		# Noir
@@ -51,16 +53,26 @@ class plateau:
 
 		for i in range(1, 9):
 			pion = self.setDeJeu.getFreePion("pion", "blanc")
-			self.matrice[(7, i)] = pion
+			self.matrice[(i, 7)] = pion
 
-		self.matrice[(8, 1)] = self.setDeJeu.getFreePion("tour","blanc")
-		self.matrice[(8, 2)] = self.setDeJeu.getFreePion("cavalier","blanc")
-		self.matrice[(8, 3)] = self.setDeJeu.getFreePion("fou","blanc")
-		self.matrice[(8, 4)] = self.setDeJeu.getFreePion("roi","blanc")
-		self.matrice[(8, 5)] = self.setDeJeu.getFreePion("reine","blanc")
-		self.matrice[(8, 6)] = self.setDeJeu.getFreePion("fou","blanc")
-		self.matrice[(8, 7)] = self.setDeJeu.getFreePion("cavalier","blanc")
+		self.matrice[(1, 8)] = self.setDeJeu.getFreePion("tour","blanc")
+		self.matrice[(2, 8)] = self.setDeJeu.getFreePion("cavalier","blanc")
+		self.matrice[(3, 8)] = self.setDeJeu.getFreePion("fou","blanc")
+		self.matrice[(4, 8)] = self.setDeJeu.getFreePion("reine","blanc")
+		self.matrice[(5, 8)] = self.setDeJeu.getFreePion("roi","blanc")
+		self.matrice[(6, 8)] = self.setDeJeu.getFreePion("fou","blanc")
+		self.matrice[(7, 8)] = self.setDeJeu.getFreePion("cavalier","blanc")
 		self.matrice[(8, 8)] = self.setDeJeu.getFreePion("tour","blanc")
+
+
+	def moveToAndTake(self, case1, case2):
+
+		piece1 = self.matrice[case1]
+
+		self.matrice[case2].setState(False)
+		self.matrice[case2].setAssociation(False)
+		self.matrice[case2] = piece1
+		self.matrice[case1] = self.setDeJeu.getFreePion("empty","")
 
 	def apercu(self):
 
