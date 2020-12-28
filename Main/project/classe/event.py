5d4edda2bf97cc59c19b69dc7cd11bbbd187a082
yohan <yohan.folliot@gmail.com>
2020-12-28 18:38:25 +0100

    Ajout de la Trace dans les event pour se souvenir des case où peut se déplacer la pièce sélectionnée à l'évènement "Click", de la sorte qu'on puisse vérifier si la case de destination est valide

--- Main/project/classe/event.py
+++ Main/project/classe/event.py
@@ -1,21 +1,24 @@
 
 class event:
 
-	def __init__(self, _id, _origin, _action, _cache=None):
+	def __init__(self, _id, _origin, _action, _cache=None, _trace=None):
 		self.id = _id
 		self.origin = _origin
 		self.action = _action
 		self.cache = _cache
+		self.trace = _trace
 
 	def getId(self): return self.id
 	def getOrigin(self): return self.origin
 	def getAction(self): return self.action
 	def getCache(self): return self.cache
+	def getTrace(self): return self.trace
 
 	def setId(self, newId): self.id = newId
 	def setOrigin(self, newOrigins): self.origin = newOrigins
 	def setAction(self, newAction): self.action = newAction
 	def setCache(self,  newCache): self.cache = newCache
+	def setTrace(self, newTrace): self.trace = newTrace
 
 	def apercu(self):
 		print("@ " + "-" * 30)
@@ -23,4 +26,5 @@ class event:
 		print("| Origin : " + str(self.origin))
 		print("| Action : " + str(self.action))
 		print("| Cache  : " + str(self.cache))
+		print("| Trace  : " + str(self.trace))
 
